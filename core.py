1239eaf9f46495d25c3d34ef181726af02ad420e
richpl <rich.lampard@gmail.com>
2019-12-08 18:14:47 +0000

    Fixed comment formatting

--- core.py
+++ core.py
@@ -28,11 +28,11 @@ from assemblytoken import AssemblyToken as Token
 
 class Core:
 
-    """
-    Initialise the core with a specified size. The core
-    will initially be filled with NULLs.
-    """
     def __init__(self, size=8000):
+        """
+        Initialise the core with a specified size. The core
+        will initially be filled with NULLs.
+        """
 
         assert isinstance(size, int)
 
@@ -40,83 +40,83 @@ class Core:
                         Token.NULL, Token.NULL]
                        for index in range(size)]
 
-    """
-    Returns the size of the core.
-    """
     @property
     def coresize(self):
+        """
+        Returns the size of the core.
+        """
 
         return len(self.__core)
 
-    """
-    Returns the opcode value (given by AssemblyToken) at the specified address.
-    """
     def opcode(self, address):
+        """
+        Returns the opcode value (given by AssemblyToken) at the specified address.
+        """
 
         if address < 0 or address > self.coresize:
             raise IndexError('Invalid address specified')
 
         return self.__core[address][0]
 
-    """
-    Returns the A-field addressing mode value (given by AssemblyToken) 
-    at the specified address.
-    """
     def a_field_mode(self, address):
+        """
+        Returns the A-field addressing mode value (given by AssemblyToken)
+        at the specified address.
+        """
 
         if address < 0 or address > self.coresize:
             raise IndexError('Invalid address specified')
 
         return self.__core[address][1]
 
-    """
-    Returns the A-field value
-    at the specified address.
-    """
     def a_field_val(self, address):
+        """
+        Returns the A-field value
+        at the specified address.
+        """
 
         if address < 0 or address > self.coresize:
             raise IndexError('Invalid address specified')
 
         return self.__core[address][2]
 
-    """
-    Returns the B-field addressing mode value (given by AssemblyToken) 
-    at the specified address.
-    """
     def b_field_mode(self, address):
+        """
+        Returns the B-field addressing mode value (given by AssemblyToken)
+        at the specified address.
+        """
 
         if address < 0 or address > self.coresize:
             raise IndexError('Invalid address specified')
 
         return self.__core[address][3]
 
-    """
-    Returns the B-field value
-    at the specified address.
-    """
     def b_field_val(self, address):
+        """
+        Returns the B-field value
+        at the specified address.
+        """
 
         if address < 0 or address > self.coresize:
             raise IndexError('Invalid address specified')
 
         return self.__core[address][4]
 
-    """
-    Puts the specified instruction in the
-    specified word position. The instruction
-    is assumed to be valid, and the original
-    contents of the word are overwritten.
-    
-    :param opcode: The numeric value representing the opcode
-    :param a_field_mode: A-field addressing mode
-    :param a_field_val: A-field value
-    :param b_field_mode: B-field addressing mode
-    :param b_field_val: B-field value
-    :param address: The address at which to insert the instruction
-    """
     def put(self,  opcode, a_field_mode, a_field_val,
             b_field_mode, b_field_val, address):
+        """
+        Puts the specified instruction in the
+        specified word position. The instruction
+        is assumed to be valid, and the original
+        contents of the word are overwritten.
+
+        :param opcode: The numeric value representing the opcode
+        :param a_field_mode: A-field addressing mode
+        :param a_field_val: A-field value
+        :param b_field_mode: B-field addressing mode
+        :param b_field_val: B-field value
+        :param address: The address at which to insert the instruction
+        """
 
         if address < 0 or address > self.coresize:
             raise IndexError('Invalid address specified')
@@ -124,10 +124,10 @@ class Core:
         self.__core[address] = [opcode, a_field_mode, a_field_val,
                                 b_field_mode, b_field_val]
 
-    """
-    Pretty prints the instruction at the specified address
-    """
     def print_instruction(self, address):
+        """
+        Pretty prints the instruction at the specified address
+        """
 
         if address < 0 or address > self.coresize:
             raise IndexError('Invalid address specified')
